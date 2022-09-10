04952e066051f94a6c9ac8d3a1965d032d1ba8d6
OUBRAYME-MUSTAFA <mustafaoubrayme@gmail.com>
2022-09-11 00:24:20 +0100

    Initial commit

---
+++ src/main/java/org/zr7/gateway/GatewayApplication.java
@@ -0,0 +1,41 @@
+package org.zr7.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	// Static Method
+
+	//@Bean
+	/*
+	RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route((r)->r.path("/customers/**").uri("http://localhost:8081/"))
+				.route((r)->r.path("/products/**").uri("http://localhost:8082/"))
+				.build();
+	}
+	 */
+
+	// With Eureka Discovery
+
+
+	// Dynamic Method
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(
+			ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
+		return new DiscoveryClientRouteDefinitionLocator(rdc, properties);
+	}
+}
